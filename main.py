f7d666e0fcedf1b7df82b3274244fb091dd5bf8d
ChavinduRansara <119533194+ChavinduRansara@users.noreply.github.com>
2024-10-17 01:31:05 +0530

    Add parsing functionality to main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from scrape import scrape, extract_content, clean_body, split_dom_content
+from parse import parse;
 
 st.title("Web Scraper")
 url = st.text_input("Enter a Website URL: ")
@@ -16,3 +17,14 @@ if st.button("Scrape Site"):
 
     with st.expander("View DOM Content"):
         st.text_area("DOM Content", clean_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_descrtiption = st.text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_descrtiption:
+            st.write("Parsing content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse(dom_chunks, parse_descrtiption)
+            st.write(result)
